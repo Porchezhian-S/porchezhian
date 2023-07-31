9530e66bc43fad265ecc80647aa0dd8213135c39
porchezhian-S <porchezhian_S@yahoo.com>
2023-07-31 09:04:50 +0530

    initial commit

---
+++ src/firstpackage/bill.java
@@ -0,0 +1,48 @@
+package firstpackage;
+
+
+
+public class bill {
+	public void add()
+	{int a=10;
+	 int b=20;
+	 int c=a+b;
+	
+		}
+
+	public static void main(String[] args) {
+		 System.out.println('c');
+		System.out.println("         TSSPDCL            "
+				          +"\n      PAYMENT RECIPT      "
+				          +"\n--------------------------"
+				          +"\nDT:25/07/2022   TIME:14:02"
+				          +"\nRCPT:12330191020     RC:12"
+				          +"\nERO:15 GACHIBOWLI         "
+				          +"\nDIST:11004 GOWLIDODDI     "
+				          +"\n--------------------------"
+				          +"\nSNO:11004 01606           "
+				          +"\nNAME:S PORCHEZHIAN        "
+				          +"\n--------------------------"
+				          +"\nCOLLEC CC AMT   : 8310.00 "
+				          +"\nCOLLEC ACD AMT  :    30 "
+				          +"\nCOLLEC RC AMT   :    0.00 "
+				          +"\nTOTAL AMOUNT    : 8310.00 "
+				          +"\nrs.Eight Thousand Three   "
+				          +"\nHundred And Ten Only.     "
+				          +"\n--------------------------"
+				          +"\nPAY MODE : CASH           ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
+
